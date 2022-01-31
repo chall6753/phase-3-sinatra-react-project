96a9ccc739b1adeb70805acade121fd058af7d4f
Charles <chall70101@gmail.com>
2022-01-30 21:23:39 -0700

    added delete function for recipes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,27 @@ require "pry"
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
- 
+  get '/recipes' do
+    Recipe.all.to_json(:include => [:ingredients, :user, :comments])
+    # binding.pry
+  end
+  get '/chefs' do
+    User.all.to_json
+  end
+  get '/ingredients' do 
+    Ingredients.all.to_json
+  end
+  get '/recipes/:id' do 
+    Recipe.find_by(params['id']).to_json(:include => [:ingredients, :user, :comments])
+   
+  end
+  delete '/recipes/:id/delete_recipe' do
+    @recipe = Recipe.find(params['id'])
+    @recipe.destroy
+    @recipe.to_json
+  end
+  get '/chefs/:id' do
+    User.find(params['id']).to_json(:include => {:recipes => {:include => :recipe_ingredients}} )
+    
+  end
 end
