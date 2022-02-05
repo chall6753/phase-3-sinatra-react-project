4fdc438360b88b85988f618630d0a6691ce04d96
Charles <chall70101@gmail.com>
2022-02-05 12:12:15 -0700

    clean up code- whitespace

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,22 +1,15 @@
 class RecipeController < ApplicationController
-
-    
-
     get '/recipes' do
         Recipe.all.to_json(include: [:ingredients, :user, :recipe_ingredients] )
-        # binding.pry
     end
-
     get '/recipes/:id' do 
         Recipe.find(params['id']).to_json(:include => [:ingredients, :user])
     end
-
     delete '/recipes/:id/delete_recipe' do
         @recipe = Recipe.find(params['id'])
         @recipe.destroy
         @recipe.to_json
     end
-
     post '/recipe_create' do 
         @user = User.find_or_create_by(username: params['username']) #first checking to see if user already exists if not create new user
         @recipe = @user.recipes.find_or_create_by(name: params['recipeName']) do |recipe| #create a new recipe that is associated to the user unless the recipe for that user already exists
