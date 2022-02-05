09a7efb4cb29720bc6f1b2568ebb122243b549f1
Charles <chall70101@gmail.com>
2022-02-05 12:14:16 -0700

    fix indentation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,8 @@
 class UserController < ApplicationController
-
     get '/users' do
         User.all.to_json(:include => [:recipes])
       end
-      
-     
-      get '/chefs/:id' do
-        User.find(params['id']).to_json(:include => {:recipes => {:include => :recipe_ingredients}} )
-      end
-
+    get '/chefs/:id' do
+      User.find(params['id']).to_json(:include => {:recipes => {:include => :recipe_ingredients}} )
+    end
 end
